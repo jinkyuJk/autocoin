8303fe7c6789cc1777d73255d4f610a219108993
jinkyuJK <jk921201@gmail.com>
2021-06-08 18:56:52 +0900

    0608 from lab

---
+++ library/cf.py
@@ -0,0 +1,17 @@
+# -*- conding: utf-8 -*-
+
+
+# db 계정
+db_id='bot'
+# db ip
+db_ip='localhost'
+# db 패스워드
+db_passwd='tj2042511!'
+
+# db port가 3306이 아닌 다른 port를 사용 하시는 분은 아래 변수에 포트에 맞게 수정하셔야합니다.
+db_port='3306'
+
+
+#upbit_api
+access_key = "rY2mxeDmWiiAxyfwWgQcJFsfCkjSTomMxel1iGZg"          # 본인 값으로 변경
+secret_key = "vxrsTBUI4K2Jx5yxr7kX7YXLuTAA3ziKAIRsvxRu"
